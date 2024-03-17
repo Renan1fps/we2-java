c302c0875fb376548df466e1dbf2b9b078d53d7e
Renan1fps <renan.sergipe68@gmail.com>
2024-03-17 20:04:22 -0300

    feat: exercicio 1 servelet

---
+++ src/main/java/Exec1.java
@@ -0,0 +1,30 @@
+
+
+import java.io.IOException;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+
+@WebServlet("/Exec1")
+public class Exec1 extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+       
+
+    public Exec1() {
+        super();
+    }
+
+
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		response.getWriter().append("<h1>Olá Mundo</h1>").append(request.getContextPath());
+	}
+
+
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		doGet(request, response);
+	}
+
+}
